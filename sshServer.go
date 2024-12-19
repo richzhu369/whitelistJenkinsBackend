b9933aa1d9c78bdf95a44c7ebf6d3b8399221794
rich <richzhu369@gmail.com>
2024-12-20 02:11:13 +0400

    输出执行的命令

--- sshServer.go
+++ sshServer.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os/exec"
 )
 
 func executeSSHCommand(server, command string) error {
 	cmd := exec.Command("ssh", "-p", "10086", "root@"+server, command)
+	log.Println("Executing command: ", cmd.String())
 	output, err := cmd.CombinedOutput()
 	if err != nil {
 		return fmt.Errorf("failed to execute command: %s, output: %s, error: %w", command, output, err)
