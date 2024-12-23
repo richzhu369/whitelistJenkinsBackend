e75d71d147c6c0c2aa76086dca9428e22254b66f
rich <richzhu369@gmail.com>
2024-12-24 01:17:01 +0400

    更改ssh超时时间

--- sshServer.go
+++ sshServer.go
@@ -10,7 +10,7 @@ import (
 
 // ssh到服务器执行命令，5秒超时
 func executeSSHCommand(server, command string) error {
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 	defer cancel()
 
 	cmd := exec.CommandContext(ctx, "ssh", "-p", "10086", "root@"+server, command)
